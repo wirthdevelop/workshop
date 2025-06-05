f54857c3c2b92a2547669b61907b04cf2749c924
wirthdevelop <120033291+wirthdevelop@users.noreply.github.com>
2025-06-05 10:12:57 +0000

    CREATE Nuxt latest

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/content',
+    '@nuxt/scripts'
+  ]
+})
